ed2b659253ce9f6486377ca0fac1ddc161bc2cdb
topzyray <topzyray@yahoo.com>
2024-04-08 09:38:39 +0100

    Add Hero and Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { VscBellDot } from 'react-icons/vsc';
+import tope from '../assets/images/tope.jpg';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -8,7 +10,9 @@ const Header = () => {
   return (
     <header className="bg-white sticky z-50 top-0 drop-shadow-xl py-3 md:py-8">
       <nav className="relative flex justify-between items-center py-1 px-6 md:px-10 lg:px-28">
-        <h1 className="text-[22px] md:text-[32px] font-extrabold gradient-text">Fickleflight</h1>
+        <h1 className="text-[22px] md:text-[32px] font-extrabold gradient-text">
+          Fickleflight
+        </h1>
         {/* Mobile Menu Hamburger */}
         <div
           onClick={() => setMenuOpen(!menuOpen)}
@@ -29,17 +33,76 @@ const Header = () => {
 
         {/* Large screen menu */}
         <ul className="hidden md:flex items-center text-base font-medium gap-8">
-          <li className='cursor-pointer hover:text-primary hover:underline'>Explore</li>
-          <li className='cursor-pointer hover:text-primary hover:underline'>Search</li>
-          <li className='cursor-pointer hover:text-primary hover:underline'>Hotels</li>
-          <li className='cursor-pointer hover:text-primary hover:underline'>Offers</li>
-          <li className='cursor-pointer'>
-            <VscBellDot />
+          <li className="cursor-pointer hover:text-primary hover:underline">
+            Explore
           </li>
-          <li className='cursor-pointer'>
-            <img alt="Img" />
+          <li className="cursor-pointer hover:text-primary hover:underline">
+            Search
+          </li>
+          <li className="cursor-pointer hover:text-primary hover:underline">
+            Hotels
+          </li>
+          <li className="cursor-pointer hover:text-primary hover:underline">
+            Offers
+          </li>
+          <li className="cursor-pointer">
+            <VscBellDot className="text-2xl" />
+          </li>
+          <li className="cursor-pointer">
+            <img src={tope} alt="Img" className="w-10 h-10 rounded-full" />
           </li>
         </ul>
+
+        {/* Mobile screen menu */}
+        {menuOpen && (
+          <ul
+            className={
+              menuOpen
+                ? `md:hidden absolute flex flex-col text-center w-full top-[3.4rem] right-0 bg-white shadow-xl px-8 py-12 space-y-6 ease-in duration-500`
+                : `top-[-100%] ease-out duration-700`
+            }
+          >
+            <NavLink
+              onClick={() => setMenuOpen(false)}
+              to="/"
+              className={({ isActive }) => (isActive ? 'text-primary' : '')}
+            >
+              <li className="hover:text-primary hover:underline cursor-pointer">
+                Explore
+              </li>
+            </NavLink>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="cursor-pointer hover:text-primary hover:underline"
+            >
+              Search
+            </li>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="cursor-pointer hover:text-primary hover:underline"
+            >
+              Hotels
+            </li>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="cursor-pointer hover:text-primary hover:underline"
+            >
+              Offers
+            </li>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="cursor-pointer mx-auto"
+            >
+              <VscBellDot className="text-2xl" />
+            </li>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="cursor-pointer mx-auto"
+            >
+              <img src={tope} alt="Img" className="w-10 h-10 rounded-full" />
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,15 +19,99 @@ const Hero = () => {
     >
       <div className="w-full flex flex-col gap-11">
         <div className="text-white text-center">
-          <h1 className="text-[61px] font-extrabold">
+          <h1 className="text-[41px] md:text-[41px] lg:text-[61px] font-extrabold">
             Let’s explore & travel the world
           </h1>
-          <p className="text-[24px] font-normal">
+          <p className="text-lg lg:text-[24px] font-normal">
             Find the best destinations and the most popular stays!
           </p>
         </div>
 
-        <div className="w-full bg-white text-black px-5 py-4 rounded-lg"></div>
+        <div className="w-full flex flex-col gap-4 bg-white text-black px-5 py-6 rounded-lg">
+          <div className="flex justify-between items-center">
+            <p className="text-lg md:text-xl font-bold sm:uppercase">
+              Search Flights
+            </p>
+            <div className="flex gap-2 sm:gap-4">
+              <label htmlFor="type">
+                <input
+                  type="radio"
+                  name="type"
+                  id="type"
+                  className="mr-1 sm:mr-2"
+                  checked
+                />
+                Return
+              </label>
+              <label htmlFor="type">
+                <input
+                  type="radio"
+                  name="type"
+                  id="type"
+                  className="mr-1 sm:mr-2"
+                />
+                One-way
+              </label>
+            </div>
+          </div>
+          <div>
+            <div className="grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-8 lg:grid-cols-7">
+              <div className="input-group md:col-span-2 md:col-start-1">
+                <label htmlFor="departure" className="label">
+                  Departure
+                </label>
+                <div className="">
+                  <input
+                    type="text"
+                    name="departure"
+                    id="departure"
+                    autoComplete="address-level2"
+                    className="input"
+                    placeholder="Departure"
+                    defaultValue="Singapore (SIN)"
+                  />
+                </div>
+              </div>
+
+              <div className="input-group md:col-span-2">
+                <label htmlFor="arrival" className="label">
+                  Arrival
+                </label>
+                <div className="">
+                  <input
+                    type="text"
+                    name="arrival"
+                    id="arrival"
+                    autoComplete="address-level1"
+                    className="input"
+                    placeholder="Arrival"
+                    defaultValue="Los Angeles (LA)"
+                  />
+                </div>
+              </div>
+
+              <div className="input-group md:col-span-2">
+                <label htmlFor="date" className="label">
+                  Date
+                </label>
+                <div className="">
+                  <input
+                    type="date"
+                    name="date"
+                    id="date"
+                    autoComplete=""
+                    className="input"
+                  />
+                </div>
+              </div>
+              <div className="md:col-span-2 lg:col-span-1">
+                <button className="btn btn-orange w-full lg:uppercase">
+                  Search Flight
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
